3b56af6bd8ef64bf474910454347954f284b88b0
Paulo <paulo.r.guarnieri@hotmail.com>
2022-09-23 04:52:26 -0300

    fix browser router

--- src/pages/kinema/index.jsx
+++ src/pages/kinema/index.jsx
@@ -17,7 +17,7 @@ const KinemaPage = () => {
 
                         <h3>Description:</h3>
                         <p translate="true">Theater network management API, with support for persisting theaters, users, theaters, movies, sessions and tickets. Coordinating publicity and ticket sales.</p>
-                        <h3>Project link: <a href="https://cap-fabio-9.herokuapp.com/" rel="noreferrer" target="_blank">Kinema API</a></h3>
+                        <h3>Project link: <a href="https://api-kinema.herokuapp.com/" rel="noreferrer" target="_blank">Kinema API</a></h3>
                         <h3>Repository: <a href="https://github.com/kenzie-academy-m5-t9-g14/cinema-api" rel="noreferrer" target="_blank">Github</a></h3>
                         <h3>Documentation: <a href="https://api-kinema.herokuapp.com/schema/swagger-ui/" rel='noreferrer' target="blank">Docs Kinema</a></h3>
                     </TextContainer1>
